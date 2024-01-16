3166805bf24b99c5e3c1c5fa4307f716590d8a75
Aisha-Yusuf <ayshaofficiel@gmail.com>
2024-01-16 21:48:20 +0300

    Migrations

--- server/models.py
+++ server/models.py
@@ -12,3 +12,18 @@ class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(200), nullable=False)
+    username = db.Column(db.String(200), nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+
+    def __init__ (self, body, username):
+        self.body = body
+        self.username = username
+
+    def to_dict(self):
+        return {
+            'id'  : self.id ,
+            'body': self.body,
+            'username' : self.username,
+            'created_at' : self.created_at.isoformat()
+        }
